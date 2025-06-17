999d00f18e3e31716f92ce33dc51d4c658ea1f1e
SnowSalam <142801902+SnowSalam@users.noreply.github.com>
2025-06-17 15:33:40 +0500

    Add category selection

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { productCard } from "./js/productCard.js";
 
 const productList = document.getElementById("product-list");
+const categorySelect = document.getElementById("category-select");
 const products = [];
+const categoriesSet = new Set();
 
 async function downloadProducts() {
   await fetch("./products.json")
@@ -14,17 +16,50 @@ async function downloadProducts() {
       return false;
     });
 
+  fillCategories(products);
   return true;
 }
 
-async function renderProductList() {
-  if (await downloadProducts()) {
-    products.forEach((item) => {
-      productList.appendChild(productCard(item));
-    });
-  } else {
-    alert("Товары не были загружены. Пожалуйста, обратитесь в техподдержку.");
-  }
+async function renderProductList(productsArr) {
+  productList.innerHTML = "";
+  productsArr.forEach((item) => {
+    productList.appendChild(productCard(item));
+  });
 }
 
-renderProductList();
+if (await downloadProducts()) {
+  renderProductList(products);
+  populateCategorySelect(categoriesSet);
+} else
+  alert("Товары не были загружены. Пожалуйста, обратитесь в техподдержку.");
+
+function fillCategories(productsArr) {
+  productsArr.forEach((product) => {
+    categoriesSet.add(product.category);
+  });
+}
+
+function populateCategorySelect(set) {
+  set.forEach((category) => {
+    const option = document.createElement("option");
+    option.className = "category-select-option";
+    option.textContent = category;
+    option.value = category;
+    categorySelect.appendChild(option);
+  });
+}
+
+categorySelect.addEventListener("change", (event) => {
+  const selectedCategory = event.target.value;
+
+  if (selectedCategory === "Все") {
+    renderProductList(products);
+    return;
+  }
+
+  renderProductList(
+    products.filter((item) => {
+      return item.category === selectedCategory;
+    })
+  );
+});
